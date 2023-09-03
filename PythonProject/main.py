8279cddac731d89909ade7f6e28b38452a9193ed
Akki5428 <132263592+Akki5428@users.noreply.github.com>
2023-09-03 19:01:24 +0530

    Add files via upload

---
+++ PythonProject/main.py
@@ -0,0 +1,74 @@
+from products import Products
+from electronics import Electronics
+from grocery import Grocery
+from furniture import Furniture
+from toys import Toys
+from cloths import Cloths
+
+e1 = Electronics("Asus TUF F15", 56000.0, 86000, 10, "Rechargable Battery")
+# e1.show_details()
+
+g1 = Grocery("Kissan Ketchup", 90.0, 135, 50, "12/24")
+# g1.show_details()
+
+while True:
+    Products.loadFromCSV()
+    print("Enter:\n1 to add new product to inventory")
+    print("2 to delete a product")
+    print("3 to view a product details")
+    print("4 to edit a product details")
+    print("5 to view the entire inventory")
+
+    print("9 to exit\n")
+    op = int(input())
+    if op == 1:
+        print("Enter:\n\t1 to add an Electronics item")
+        print("\t2 to add a Furniture")
+        print("\t3 to add Grocery")
+        print("\t4 to add Cloth")
+        print("\t5 to add Toy")
+        item_type = int(input())    # 3
+        lookup = {
+            1 : Electronics.addNewItem,
+            2 : Furniture.addNewItem,
+            3 : Grocery.addNewItem,
+            4 : Cloths.addNewItem,
+            5 : Toys.addNewItem
+        }
+        lookup[item_type]()
+
+    elif op == 2:
+        index = Products.showInventory()
+        if not index:
+            print("Please add a product first!")
+        else:
+            removed_product = Products.all_products[index]
+            Products.all_products[index] = None
+            print(f"{removed_product.name} has been successfully removed!\n")
+
+    elif op == 3:
+        index = Products.showInventory()
+        if not index:
+            print("Please add a product first!")
+        else:
+            Products.all_products[index].show_details()
+
+    elif op == 4:
+        index = Products.showInventory()
+        if not index:
+            print("Please add a product first!")
+        else:
+            Products.all_products[index].editDetails()
+
+    elif op == 5:
+        Products.showInventory()
+
+    elif op == 9:
+        Products.writeToCSV()
+        break
+
+    else:
+        print("Invalid operation, please try again...")
+
+
+Products.loadFromCSV()
---
+++ PythonProject/products.py
@@ -0,0 +1,140 @@
+import csv
+import datetime
+from abc import ABC, abstractmethod      # abc: abstract base classes, ABC: Abstract Base Class
+"""
+Abstraction:
+Purpose 1: To disallow object creation in a particular class
+Purpose 2: To make some of the methods compulsory to all the child classes to have
+"""
+
+class Products(ABC):
+    all_products = []
+    country = "India"
+    # category_code = "P"
+    # category = "Product"
+
+    def __init__(self, name:str, cost_price, mrp, quantity:int):
+        assert isinstance(name, str), "Name should be a string..."
+        self.name = name
+
+        assert isinstance(cost_price, float), "Cost Price should be a Float..."
+        self.__cost_price = cost_price              # Private Variable
+        assert isinstance(mrp, float) or isinstance(mrp, int), "MRP should be a Float..."
+        self._mrp = mrp                             # Protected Variable
+        
+        assert isinstance(quantity, int), "Qunatity should be integer."
+        assert quantity > 0, "Quantity should be greater than 0!"
+        self.quantity = quantity
+        if None in self.all_products:
+            index = self.all_products.index(None)
+            Products.all_products[index] = self
+        else:
+            Products.all_products.append(self)
+        self.generateBarcode()
+
+
+
+
+    def generateBarcode(self):
+        # Format: YYYYMMCXXXX
+        """
+        YYYY: Year of purchase in 4 digits (to be fetched from the system date using datetime module)
+        MM: Month of purchase in 2 digits (to be fetched from the system date using datetime module)
+        C: category_code (E/G/F/T/C)
+        XXXX: Index number in the list + 100
+        """
+        purchase_year = datetime.datetime.today().year
+        purchase_month = datetime.datetime.today().month
+        index = str(Products.all_products.index(self) + 101)
+        self.barcode = str(purchase_year)+str(purchase_month).zfill(2)+ self.category_code + index
+        # barcode: 202307E102
+
+    @abstractmethod
+    def show_details(self):
+        print(f"------------- Details of {self.name} -------------")
+        print("Category:", self.category)
+        print("Cost price:", self.__cost_price)     # private variable
+        print("MRP:", self._mrp)                    # protected variable
+        print("Stock:", self.quantity)
+        print("Barcode:", self.barcode)
+
+    @staticmethod
+    @abstractmethod
+    def addNewItem():
+        print("Enter the following details:")
+        name = input("Name: ")
+        cost_price = float(input("Cost Price: "))
+        mrp = float(input("MRP: "))
+        quantity = int(input("Quantity: "))
+        return name, cost_price, mrp, quantity
+    
+    @staticmethod
+    def showInventory():
+        if len(Products.all_products) == 0:
+            print("Your inventory is empty!")
+            return False
+        print("SrNo\t\tItem Name")
+        for item in Products.all_products:
+            print(f"{item.barcode}\t{item.name}")
+        barcode = input("Enter barcode no: ")
+        index = int(barcode[-3:]) - 101
+        return index
+
+    def editDetails(self):
+        print("Enter new details (Press 'Enter' to keep old detail):")
+        print("Field\tOld Value\tNew Value".expandtabs(20))
+        name = input(f"Name\t{self.name}:\t".expandtabs(20))
+        if name != "": self.name = name
+
+        category = input(f"Category\t{self.category}:\t".expandtabs(20))
+        if category != "": 
+            print("Sorry, cannot change category from here. You need to delete this object and create a new one in that category.")
+        
+        cost_price = input(f"Cost price\t{self.cost_price}:\t".expandtabs(20))
+        if cost_price != "": self.cost_price = float(cost_price)
+
+        mrp = input(f"MRP\t{self.mrp}:\t".expandtabs(20))
+        if mrp != "": self.mrp = float(mrp)
+
+        quantity = input(f"Stock\t{self.quantity}:\t".expandtabs(20))
+        if quantity != "": self.quantity = int(quantity)
+
+        month = input(f"Month\t{self.barcode[4 : 6]}:\t".expandtabs(20)).zfill(2)
+        if month != "":
+            self.barcode = self.barcode[ : 4] + month + self.barcode[6 : ]
+        year = input(f"Year\t{self.barcode[ : 4]}:\t".expandtabs(20))
+
+        if year != "":
+            self.barcode = year + self.barcode[4 : ]
+
+
+    @staticmethod
+    def writeToCSV():
+        with open('database.csv', 'w') as f:
+            for obj in Products.all_products:
+                line = ""
+                # for key, value in obj.__dict__.items():
+                    # line = line + f"({key}, {value})" + ","
+                for property in obj.__dict__.items():
+                    line = line + str(property) + ","
+                line = line[0 : len(line)-1]
+                line = f"(category_code, {obj.category_code})," + line
+                f.write(line + "\n")
+
+    @staticmethod
+    def loadFromCSV():
+        with open('database.csv', 'r') as f:
+            raw_objects = csv.reader(f)
+            # print(raw_objects)
+            for line in raw_objects:
+                print(line)
+
+
+if __name__ == '__main__' :
+    # p1 = Products("1234", 35.5, 50.5, 10)
+    # p1.show_details()
+    # p2 = Products("Sample product 1", 35.5, 50.5, 10)
+    # p2.show_details()
+    # s1 = 123
+    # print(isinstance(s1, str))
+    pass
